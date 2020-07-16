545025f415a0f47b7bed43f629468f45fa60e44f
Anantho Pranowo <refl3ct0r@gmail.com>
2020-07-16 09:53:11 +0700

    Create update_named.sh

---
+++ update_named.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+wget -O /tmp/root.hints https://www.internic.net/domain/named.root
+mv /tmp/root.hints /etc/unbound/
+systemctl restart unbound
