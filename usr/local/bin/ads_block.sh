17f38fa22d66cee2c3b9430d5e3e6cd6bd98f301
Anantho Pranowo <refl3ct0r@gmail.com>
2020-08-07 10:33:10 +0700

    Remove local-zone: "0.0.0.0" and blank line

--- usr/local/bin/ads_block.sh
+++ usr/local/bin/ads_block.sh
@@ -5,5 +5,7 @@ rm /tmp/ads.conf*
 wget -O /tmp/hosts https://block.energized.pro/ultimate/formats/hosts.txt
 echo "server:" > /tmp/ads.conf
 cat /tmp/hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" always_nxdomain"}' >> /tmp/ads.conf
-cp /tmp/ads.conf /etc/unbound/zones/energized-ultimate.conf
+sed -e 's/\<local-zone: "0.0.0.0" always_nxdomain\>//g' /tmp/ads.conf > /tmp/ads.conf.1
+sed -i '/^$/d' /tmp/ads.conf.1
+cp /tmp/ads.conf.1 /etc/unbound/zones/energized-ultimate.conf
 systemctl restart unbound
