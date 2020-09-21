ce1b9a30f47753613246c46b255b36401fa98dd8
Anantho Pranowo <refl3ct0r@gmail.com>
2020-09-21 08:23:45 +0700

    Update ads_block.sh
    change filter to basic energized

--- usr/local/bin/ads_block.sh
+++ usr/local/bin/ads_block.sh
@@ -2,10 +2,10 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 rm /tmp/hosts*
 rm /tmp/ads.conf*
-wget -O /tmp/hosts https://block.energized.pro/ultimate/formats/hosts.txt
+wget -O /tmp/hosts https://block.energized.pro/basic/formats/hosts.txt
 echo "server:" > /tmp/ads.conf
 cat /tmp/hosts | grep '^0\.0\.0\.0' | awk '{print "local-zone: \""$2"\" always_nxdomain"}' >> /tmp/ads.conf
 sed -e 's/\<local-zone: "0.0.0.0" always_nxdomain\>//g' /tmp/ads.conf > /tmp/ads.conf.1
 sed -i '/^$/d' /tmp/ads.conf.1
 cp /tmp/ads.conf.1 /etc/unbound/zones/energized-ultimate.conf
-systemctl restart unbound
+systemctl restart 
